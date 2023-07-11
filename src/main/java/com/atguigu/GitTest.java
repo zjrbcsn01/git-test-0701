6d3429987c77c7f086268c8eb7e6e427faef80ff
zjr <zjr@atguigu.com>
2023-07-11 13:21:44 +0800

    第一次commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author zjr
+ * @create 2023-07-11
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("test...");
+    }
+}
